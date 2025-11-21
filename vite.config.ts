48965c7108063860e163c987f7ff8847f0211d95
Wow Rakibul <40rakib70@gmail.com>
2025-11-21 19:38:30 +0600

    setup v2

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { viteSingleFile } from 'vite-plugin-singlefile';
 import path from 'path';
 
 export default defineConfig({
-    plugins: [react(), viteSingleFile()],
+    plugins: [react()],
     build: {
         target: 'esnext',
         assetsInlineLimit: 100000000,
